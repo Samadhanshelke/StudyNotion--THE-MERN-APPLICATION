a1fb83dd19e102c2b1983b7955d4f8404d6c6409
Samadhanshelke <96407937+Samadhanshelke@users.noreply.github.com>
2023-09-13 13:11:59 +0530

    small change in origin

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"*",
+        origin:"https://studynotion-the-mern-application.onrender.com",
         Credential:true,
     })
 )
